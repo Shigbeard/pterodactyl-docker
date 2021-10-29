09d68b4a509e248dad9aa9688f0cd18477de080c
HotaruBlaze <DeriousHD@gmail.com>
2021-10-29 16:18:59 +0000

    Push All Changes

--- manifest/panel/entrypoint.sh
+++ manifest/panel/entrypoint.sh
@@ -5,38 +5,34 @@
 ###
 
 # Prep Container for usage
-function init {
-    # Create the storage/cache directories
-    if [ ! -d /data/storage ]; then
-        mkdir -p /data/storage
-        cat .storage.tmpl | while read line; do
-            mkdir -p "/data/${line}"
-        done
-        chown -R nginx:nginx /data/storage
-    fi
+function initContainer {
+    # Check if MySQL is up and running
+    echo "[init] Waiting for database connection..."
+    i=0
+    until wait-for -q -t 15 $DB_HOST:$DB_PORT; do
+        echo "[init] Database connection timeout"
 
-    if [ ! -d /data/cache ]; then
-        mkdir -p /data/cache
-        chown -R nginx:nginx /data/cache
-    fi
-
-    # destroy links (or files) and recreate them
-    rm -rf storage
-    ln -s /data/storage storage
-
-    rm -rf bootstrap/cache
-    ln -s /data/cache bootstrap/cache
-
-    rm -rf .env
-    ln -s /data/pterodactyl.conf .env
+        # wait for 5 seconds before check again
+        sleep 5
+        i=`expr $i + 1`
+        if [ "$i" = "5" ]; then
+            echo "[init] Connection threshold reached, unable to contact MySQL (Is MySQL Running?)"
+            exit 3
+        fi
+    done
 }
 
 # Runs the initial configuration on every startup
 function startServer {
+    echo ""
+    cat .storage.tmpl | while read line; do
+        mkdir -p "/data/${line}"
+    done
 
-    # Initial setup
+    # Generate config file if it doesnt exist
     if [ ! -e /data/pterodactyl.conf ]; then
-        echo "Running first time setup..."
+        echo ""
+        echo "[setup] Generating Application Key..."
 
         # Generate base template
         touch /data/pterodactyl.conf
@@ -50,56 +46,71 @@ function startServer {
         echo "" >> /data/pterodactyl.conf
         echo "APP_KEY=SomeRandomString3232RandomString" >> /data/pterodactyl.conf
 
-        sleep 5
-
-        echo ""
-        echo "Generating key..."
         sleep 1
         php artisan key:generate --force --no-interaction
 
-        echo ""
-        echo "Creating & seeding database..."
-        sleep 1
-        php artisan migrate --force
-        php artisan db:seed --force
+        echo "[setup] Application Key Generated"
     fi
+    echo ""
+    echo "[setup] Clearing cache/views..."
+    
+    php artisan view:clear
+    php artisan config:clear
 
-    # Allows Users to give MySQL/cache sometime to start up.
-    if [[ "${STARTUP_TIMEOUT}" -gt "0" ]]; then
-        echo "Starting Pterodactyl ${PANEL_VERSION} in ${STARTUP_TIMEOUT} seconds..."
-        sleep ${STARTUP_TIMEOUT}
-    else 
-        echo "Starting Pterodactyl ${PANEL_VERSION}..."
-    fi
+    echo ""
+    echo "[setup] Migrating/Seeding database..."
 
-    if [ "${SSL}" == "true" ]; then
+    php artisan migrate --seed --force
+
+    # Restore /data directory ownership to nginx.
+    chown -R nginx:nginx /data/
+    
+    # Checks if SSL certificate and key exists, otherwise default to http traffic
+    if [ -f "${SSL_CERT}" ] && [ -f "${SSL_CERT_KEY}" ]; then
         envsubst '${SSL_CERT},${SSL_CERT_KEY}' \
         < /etc/nginx/templates/https.conf > /etc/nginx/conf.d/default.conf
     else
-        echo "[Warning] Disabling HTTPS"
+        echo "[setup] Warning: SSL Certificate was not specified or doesnt exist, using HTTP."
         cat /etc/nginx/templates/http.conf > /etc/nginx/conf.d/default.conf
     fi
 
-    # Determine if workers should be enabled or not
-    if [ "${DISABLE_WORKERS}" != "true" ]; then
-        /usr/sbin/crond -f -l 0 &
-        php /var/www/html/artisan queue:work database --queue=high,standard,low --sleep=3 --tries=3 &
-    else 
-        echo "[Warning] Disabling Workers (pteroq & cron); It is recommended to keep these enabled unless you know what you are doing."
-    fi
+    echo "--- Starting Pterodactyl Panel: ${VERSION} ---"
 
-    /usr/sbin/php-fpm7 --nodaemonize -c /etc/php7 &
+    # Run these as jobs and monitor their pid status
+    /usr/sbin/php-fpm7 --nodaemonize -c /etc/php7 & php_service_pid=$!
+    /usr/sbin/nginx -g "daemon off;" & nginx_service_pid=$!
 
-    exec /usr/sbin/nginx -g "daemon off;"
+    # Monitor Child Processes
+    while ( true ); do
+        if ! kill -0 "$php_service_pid" 2>/dev/null; then
+            echo "[php] service is no longer running! exiting..."
+            sleep 5
+            wait "$php_service_pid";
+            exit 1
+        fi
+        if ! kill -0 "$nginx_service_pid" 2>/dev/null; then
+            echo "[nginx] service is no longer running! exiting..."
+            sleep 5
+            wait "$nginx_service_pid"; 
+            exit 2
+        fi
+        sleep 1
+    done;
 }
 
 ## Start ##
 
-init
+initContainer
 
 case "${1}" in
     p:start)
         startServer
+        ;;
+    p:worker)
+        exec php /var/www/html/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+        ;;
+    p:cron)
+        exec /usr/sbin/crond -f -l 0
         ;;
     *)
         exec ${@}
