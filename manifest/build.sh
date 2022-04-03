429486c43c202699d4e9073c420877663df97c39
Phoenix <DeriousHD@gmail.com>
2022-04-03 02:36:42 +0100

    Update Deployment for gameinfo builds

--- manifest/build.sh
+++ manifest/build.sh
@@ -4,11 +4,11 @@
 . ./version.txt
 
 # Build panel
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$PANEL_VERSION -t mrflutters/pterodactyl-panel:$PANEL_VERSION ./panel
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$PANEL_VERSION -t shigbeard/pterodactyl-panel:$PANEL_VERSION ./panel
 
 # Build Daemon
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$DAEMON_VERSION -t mrflutters/pterodactyl-daemon:$DAEMON_VERSION ./daemon
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$DAEMON_VERSION -t shigbeard/pterodactyl-daemon:$DAEMON_VERSION ./daemon
 
 # Build Dedicated SFTP Server
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$SFTP_VERSION -t mrflutters/pterodactyl-sftp:$SFTP_VERSION ./sftp
+# DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$SFTP_VERSION -t shigbeard/pterodactyl-sftp:$SFTP_VERSION ./sftp
 
